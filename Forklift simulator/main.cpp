6608bc3a06fa252a6180ceb1be6e8478fad8ea03
jacobotapia <jacob.et.cetera@gmail.com>
2017-03-26 18:08:04 -0600

    Initial Commit

---
+++ Forklift simulator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Forklift simulator
+//
+//  Created by Jacobo Tapia on 26/03/17.
+//  Copyright © 2017 Jacobo Tapia. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
